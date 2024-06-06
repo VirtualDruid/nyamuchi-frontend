eabe38b5be5d8b42359475b65de9dd2908f37390
VirtualDruid <virtualdruid70@gmail.com>
2024-06-06 22:12:58 +0800

    vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,7 @@ const nextConfig = {
     images: {
         domains: ['34.84.210.183'],
     },
-    output: 'export',
-    basePath: '/nyamuchi-frontend/docs',
+    output: 'export'
 };
 
 export default nextConfig;
