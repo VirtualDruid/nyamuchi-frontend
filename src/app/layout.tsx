bb6c199d3d331114fa3a75a00dcae85d9c805863
VirtualDruid <virtualdruid70@gmail.com>
2024-06-17 00:44:21 +0800

    update: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@
 import { Inter } from "next/font/google";
 //import "./globals.css";
 
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -13,7 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en" style={{backgroundColor:"rgb(51, 129, 175)", height:"100%"}}>
       <title>MYGO</title>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
